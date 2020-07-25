07ae45c9573cbd1f9cabecf907b6f3d3eec0bc5b
Aleksandr Kolot <aleksandrkolot7@gmail.com>
2020-07-25 10:32:44 +0300

    coment

--- task.js
+++ task.js
@@ -1,4 +1,28 @@
-let base = prompt('давай число');
-base = Number(base);
+// let base = prompt('давай число');
+// base = Number(base);
 
-console.log(base);
+// console.log(base);
+
+// let power = prompt('давай степень');
+// power = Number(power);
+// console.log(power);
+
+// const result = base ** power;
+// console.log(result);
+// let clientCounter = 18;
+// const maxClients = 25;
+
+// while (clientCounter <= maxClients) {
+//   console.log(clientCounter);
+//   clientCounter += 1;
+// }
+
+// const number = 10;
+// Number('25px'
+// // for (let i = 0; i < number; i += 1) {
+// //   if (i % 2 === 0) {
+// //     continue;
+// //   }
+
+console.log(10 + 20 + '30');
+// }
